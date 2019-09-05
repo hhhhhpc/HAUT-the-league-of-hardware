f3535f9209790f23198f7724fea74259b981796d
HAUT180158 <442490691@qq.com>
2019-09-05 11:19:53 +0800

    update re

--- website/msgapp/views.py
+++ website/msgapp/views.py
@@ -11,18 +11,18 @@ def msgproc(request):
         phone = request.POST.get("phone", None)
         major = request.POST.get("major", None)
         if re.match(u"[\u4e00-\u9fa5]+", name) is None:
-            with open('msgdata.txt', 'a+') as f:
+            with open('^msgdata.txt$', 'a+') as f:
                 f.write("{}--{}--{}--{}\n".format(name, phone, major, time.strftime("%Y-%m-%d %H:%M:%S")))
                 f.write("名字输入错误")
                 f.write("\n")
             return render(request,'failed.html')
-        if re.match("\d{11}", phone) is None:
+        if re.match("^\d{11}$", phone) is None:
             with open('msgdata.txt', 'a+') as f:
                 f.write("{}--{}--{}--{}\n".format(name, phone, major, time.strftime("%Y-%m-%d %H:%M:%S")))
                 f.write("电话输入错误")
                 f.write("\n")
             return render(request,'failed.html')
-        if re.match(u"[\u4e00-\u9fa5]+", major) is None:
+        if re.match(u"^[\u4e00-\u9fa5]+$", major) is None:
             with open('msgdata.txt', 'a+') as f:
                 f.write("{}--{}--{}--{}\n".format(name, phone, major, time.strftime("%Y-%m-%d %H:%M:%S")))
                 f.write("专业输入错误")
@@ -32,6 +32,6 @@ def msgproc(request):
         with open('msgdata.txt', 'a+') as f:
             f.write("{}--{}--{}--{}--\n".format(name, phone,\
                             major, time.strftime("%Y-%m-%d %H:%M:%S")))
-        return render(request,'succeed.html')
+        return render(request, 'succeed.html')
     if request.method == "GET":
         return render(request, "MsgSingleWeb.html", {"data":datalist})
