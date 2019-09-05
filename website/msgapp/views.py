d3a2c1f4138f714229c7464968c1e3d925d2cd91
HAUT180158 <442490691@qq.com>
2019-09-05 17:18:55 +0800

    remove failed message

--- website/msgapp/views.py
+++ website/msgapp/views.py
@@ -11,22 +11,10 @@ def msgproc(request):
         phone = request.POST.get("phone", None)
         major = request.POST.get("major", None)
         if re.match(u"[\u4e00-\u9fa5]+", name) is None:
-            with open('^msgdata.txt$', 'a+') as f:
-                f.write("{}--{}--{}--{}\n".format(name, phone, major, time.strftime("%Y-%m-%d %H:%M:%S")))
-                f.write("名字输入错误")
-                f.write("\n")
-            return render(request,'failed.html')
+            return render(request, 'failed.html')
         if re.match("^\d{11}$", phone) is None:
-            with open('msgdata.txt', 'a+') as f:
-                f.write("{}--{}--{}--{}\n".format(name, phone, major, time.strftime("%Y-%m-%d %H:%M:%S")))
-                f.write("电话输入错误")
-                f.write("\n")
             return render(request,'failed.html')
         if re.match(u"^[\u4e00-\u9fa5]+$", major) is None:
-            with open('msgdata.txt', 'a+') as f:
-                f.write("{}--{}--{}--{}\n".format(name, phone, major, time.strftime("%Y-%m-%d %H:%M:%S")))
-                f.write("专业输入错误")
-                f.write("\n")
             return render(request,'failed.html')
 
         with open('msgdata.txt', 'a+') as f:
